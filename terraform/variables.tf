ccbd18c1745315b5231d675bfd26bf9fb329e7dc
muxun <muxund@gmail.com>
2019-01-23 22:50:16 +0300

    terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,21 @@
 variable project {
-	description = "Project ID"
+  description = "Project ID"
 }
 
 variable region {
-	description = "Region"
-	default = "europe-west1"
+  description = "Region"
+  default     = "europe-west1"
 }
 
 variable public_key_path {
-	description = "Path to the public key used for ssh acess"
-
+  description = "Path to the public key used for ssh acess"
 }
 
 variable disk_image {
-	description = "Disk image"
+  description = "Disk image"
 }
 
 variable zone {
-	description = "zone of creating instance"
-	default = "europe-west1-b"
+  description = "zone of creating instance"
+  default     = "europe-west1-b"
 }
