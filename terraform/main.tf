21eb38dd6fb200bf2dd158b0393061cd07426be0
muxun <muxund@gmail.com>
2019-01-23 17:40:19 +0300

    add firewall rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,7 @@ resource "google_compute_instance" "app" {
 	name 		= "reddit-app"
 	machine_type 	= "g1-small"
 	zone		= "europe-west1-b"
+	tags = ["reddit-app"]
 	#определение загрузочного диска
 	boot_disk {
 		initialize_params {
@@ -24,4 +25,21 @@ resource "google_compute_instance" "app" {
 	metadata {
 		ssh-keys = "muxund:${file("~/.ssh/id_rsa.pub")}"
 	}
+}
+
+
+resource "google_compute_firewall" "firewall_puma" {
+	name	= "allow-puma-default"
+	#название сети , в которой действует правило
+	network = "default"
+	# что разрешаем 
+	allow {
+		protocol = "tcp"
+		ports	 = ["9292"]
+	}
+	# откуда разрешаем доступ
+	source_ranges = ["0.0.0.0/0"]
+	# правила дл яинстансов с тегами
+	target_tags = ["reddit-app"]
+
 }
