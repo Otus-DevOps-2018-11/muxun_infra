ca563c73b892ec9ff2a3b939456b355ec0aa1d4b
muxun <muxund@gmail.com>
2019-01-25 18:00:05 +0300

    create s3 buckets

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,18 @@
+provider "google" {
+	version = "1.4.0"
+	project = "${var.project}"
+	region  = "${var.region}" 
+}
+
+
+module "storage-bucket" {
+	source = "SweetOps/storage-bucket/google"
+	version = "0.1.1"
+
+	name = ["st_bucket1","st-bucket2"]
+
+}
+
+output storage-bucket_url {
+	value = "${module.storage-bucket.url}"
+}
